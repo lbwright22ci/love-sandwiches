efaf52316dd5e7302e7aaafa45e063b83dbf466a
lbwright22ci <lbwright22@gmail.com>
2026-03-09 11:50:29 +0000

    collect last 5 entries of sales data for each sandwich

--- run.py
+++ run.py
@@ -82,6 +82,20 @@ def calculate_surplus_data(sales_row):
         surplus_data.append(surplus)
     return surplus_data
 
+def get_last_five_entries_sales():
+    """
+    Collects columns of data from the sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the sate as
+    a list of lists
+    """
+    sales = SHEET.worksheet("sales")
+    columns = []
+
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    
+    return columns
 
 def main():
     """ run all program functions"""
@@ -92,4 +106,6 @@ def main():
     update_spreadsheet(new_surplus_data, "surplus")
 
 print("Welcome to Love Sandwiches data automation")
-main()
+#main()
+
+sales_columns = get_last_five_entries_sales()
