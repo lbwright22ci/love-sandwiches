1c0c897bce123bb45dde92c7fb379909490f62d6
lbwright22ci <lbwright22@gmail.com>
2026-03-09 10:10:02 +0000

    updates sales worksheet with new data

--- run.py
+++ run.py
@@ -36,7 +36,7 @@ def get_sales_data():
             print("Data is valid!")
             break
 
-        return sales_data
+    return sales_data
 
 
 def validate_data(values):
@@ -55,4 +55,16 @@ def validate_data(values):
         return False
     return True
 
+def update_sales_worksheet(data):
+    """
+    update sales worksheet by adding new row with the list of data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
 data = get_sales_data()
+
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
